d5beea5576422d766db8f8ffae3ca42332ce3b49
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-10-28 16:32:23 -0300

    Components, SignIn e SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,72 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
+    const passwordRef = useRef();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit() {
+        console.log(email, password);
+    }
+
     return (
         <Background>
-            <Text>SignIn</Text>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha secreta"
+                        ref={passwordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+
+                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => navigation.navigate('SignUp')}>
+                    <SignLinkText>Criar conta gratuita</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 }
+
+SignIn.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+    }).isRequired,
+};
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,87 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
-    return <View />;
+import Background from '~/components/Background';
+
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit() {
+        console.log(name, email, password);
+    }
+
+    return (
+        <Background>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="person-outline"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Nome Completo"
+                        returnKeyType="next"
+                        onSubmitEditing={() => emailRef.current.focus()}
+                        value={name}
+                        onChangeText={setName}
+                    />
+
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                        ref={emailRef}
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha secreta"
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+
+                    <SubmitButton onPress={handleSubmit}>
+                        Criar conta
+                    </SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => navigation.navigate('SignIn')}>
+                    <SignLinkText>Já tenho conta</SignLinkText>
+                </SignLink>
+            </Container>
+        </Background>
+    );
 }
+
+SignUp.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+    }).isRequired,
+};
